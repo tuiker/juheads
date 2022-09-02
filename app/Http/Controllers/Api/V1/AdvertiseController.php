c760be395afeb5704e0b336d5859cdd2d15134b3
longfei <1371986272@qq.com>
2022-09-02 15:44:28 +0800

    集成jiannei/laravel-response和jiannei/laravel-enum api统一响应

--- app/Http/Controllers/Api/V1/AdvertiseController.php
+++ app/Http/Controllers/Api/V1/AdvertiseController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Resources\AdvertiseResource;
 use App\Models\Advertise;
 use Illuminate\Http\Request;
+use Jiannei\Response\Laravel\Support\Facades\Response;
 
 class AdvertiseController extends Controller
 {
     public function index()
     {
         $advertise = Advertise::paginate(30);
-        return AdvertiseResource::collection($advertise);
+        return Response::success(AdvertiseResource::collection($advertise));
     }
 }
